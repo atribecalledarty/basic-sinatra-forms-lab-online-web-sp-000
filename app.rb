be3f0b0506b12546470dccfe9c90513ab2be3de0
Arthur Song <arth3rs0ng@gmail.com>
2019-05-13 21:42:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
   get '/newteam' do
     erb :newteam
   end
-
+  
+  post '/team' do
+    @team_name = params["team_name"]
+    @coach = params["coach"]
+    @point_gaurd = params["point_gaurd"]
+    @shooting_gaurd = params["shooting_gaurd"]
+    @small_forward = params["small_forward"]
+    @power_forward = params["power_forward"]
+    @center = params["center"]
+    
+    erb :team
+  end
 end
